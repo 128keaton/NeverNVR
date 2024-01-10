48f289b0d68600501dd83963098cbba6d30cc1f6
Keaton Burleson <keaton@vipindoorstorage.com>
2024-01-10 15:15:58 -0600

    Fixes initial user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -16,13 +16,13 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 @ApiTags('Users')
-@UseGuards(AuthGuard(['jwt', 'api-key']))
 @ApiSecurity('api-key')
 @ApiBearerAuth()
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post()
+  @UseGuards(AuthGuard(['jwt', 'api-key']))
   create(@Body() request: CreateUserRequest) {
     return this.usersService.create(request);
   }
@@ -40,6 +40,7 @@ export class UsersController {
     name: 'search',
     required: false,
   })
+  @UseGuards(AuthGuard(['jwt', 'api-key']))
   users(
     @Query('pageSize') pageSize: number,
     @Query('pageNumber') pageNumber: number,
@@ -54,16 +55,19 @@ export class UsersController {
   }
 
   @Get(':userID')
+  @UseGuards(AuthGuard(['jwt', 'api-key']))
   user(@Param('userID') id: string) {
     return this.usersService.findOneByID(id);
   }
 
   @Patch(':userID')
+  @UseGuards(AuthGuard(['jwt', 'api-key']))
   update(@Param('userID') id: string, @Body() request: UpdateUserRequest) {
     return this.usersService.update(id, request);
   }
 
   @Delete(':userID')
+  @UseGuards(AuthGuard(['jwt', 'api-key']))
   delete(@Param('userID') id: string) {
     return this.usersService.delete(id);
   }
