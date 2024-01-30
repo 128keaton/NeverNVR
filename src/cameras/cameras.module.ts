3a3cff9eea0d6b3c16c0b8d734cba2aa4277b100
Keaton Burleson <keaton@vipindoorstorage.com>
2024-01-30 10:41:42 -0600

    Caching for camera previews

--- src/cameras/cameras.module.ts
+++ src/cameras/cameras.module.ts
@@ -9,6 +9,11 @@ import { BullModule } from '@nestjs/bull';
 import { GatewaysModule } from '../gateways/gateways.module';
 import { S3Module } from '../services/s3/s3.module';
 import { GatewayEventsModule } from '../gateway-events/gateway-events.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import type { RedisClientOptions } from 'redis';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as redisStore from 'cache-manager-redis-store';
+import { StoreConfig } from 'cache-manager';
 
 @Module({
   controllers: [CamerasController],
@@ -22,6 +27,17 @@ import { GatewayEventsModule } from '../gateway-events/gateway-events.module';
     GatewaysModule,
     S3Module,
     GatewayEventsModule,
+    CacheModule.registerAsync<RedisClientOptions>({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const url = configService.get('REDIS_URL');
+        return {
+          store: redisStore,
+          url,
+        } as StoreConfig;
+      },
+    }),
   ],
   exports: [CamerasService, CamerasGateway],
 })
